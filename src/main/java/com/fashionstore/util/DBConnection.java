7d3aa4c5e3b5d390e84873b5c1dc84fa5fac4255
ChaithanyaBalapuram <chaithanyabalapuram14@gmail.com>
2026-06-30 17:29:00 +0530

    updated database

--- src/main/java/com/fashionstore/util/DBConnection.java
+++ src/main/java/com/fashionstore/util/DBConnection.java
@@ -1,32 +1,31 @@
 package com.fashionstore.util;
 
-import java.io.InputStream;
 import java.sql.Connection;
 import java.sql.DriverManager;
-import java.util.Properties;
 
 public class DBConnection {
 
-	private static final String URL = System.getenv("DB_URL");
-	private static final String USERNAME = System.getenv("DB_USER");
-	private static final String PASSWORD = System.getenv("DB_PASSWORD");
-
-    static {
-        Properties props = new Properties();
-        try (InputStream in = DBConnection.class.getClassLoader().getResourceAsStream("db.properties")) {
-            if (in != null) {
-                props.load(in);
-            }
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-       
-    }
+    private static final String URL = System.getenv("DB_URL");
+    private static final String USERNAME = System.getenv("DB_USER");
+    private static final String PASSWORD = System.getenv("DB_PASSWORD");
 
     public static Connection getConnection() {
         try {
             Class.forName("com.mysql.cj.jdbc.Driver");
-            return DriverManager.getConnection(URL, USERNAME, PASSWORD);
+
+            if (URL == null || USERNAME == null || PASSWORD == null) {
+                throw new RuntimeException("Database environment variables are not set.");
+            }
+
+            System.out.println("URL: " + URL);
+            System.out.println("Username: " + USERNAME);
+
+            Connection conn = DriverManager.getConnection(URL, USERNAME, PASSWORD);
+
+            System.out.println("Database Connected Successfully!");
+
+            return conn;
+
         } catch (Exception e) {
             e.printStackTrace();
             return null;
